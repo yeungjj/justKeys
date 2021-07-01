68f7987959c0ac78da81efe68939801ae57b26ab
justin <justayeung4@gmail.com>
2021-07-01 00:31:09 -0400

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("shouldnt appear");
+        System.out.println("bye");
+    }
+}
